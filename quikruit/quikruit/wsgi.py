e5209df569704bac5711ffd2ed32e63427aa1f62
JordanField <j.field.2@warwick.ac.uk>
2019-02-08 21:53:42 +0000

    As below.

--- quikruit/quikruit/wsgi.py
+++ quikruit/quikruit/wsgi.py
@@ -8,10 +8,6 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-sys.path.append('/opt/bitnami/apps/django/django_projects/quikruit')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/quikruit/egg_cache")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quikruit.settings')
 application = get_wsgi_application()
