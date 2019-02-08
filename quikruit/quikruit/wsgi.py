63e9dfc363c0e76fed12a3079cf18e5d18234093
JordanField <j.field.2@warwick.ac.uk>
2019-02-08 19:33:25 +0000

    Whoops, forgot to change something.

--- quikruit/quikruit/wsgi.py
+++ quikruit/quikruit/wsgi.py
@@ -12,6 +12,6 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append('/opt/bitnami/apps/django/django_projects/PROJECT')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/myproject/egg_cache")
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/quikruit/egg_cache")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quikruit.settings')
 application = get_wsgi_application()
