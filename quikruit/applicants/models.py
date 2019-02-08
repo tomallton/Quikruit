c67a017246f08c8eff3e20b0b2b080400e82e893
JordanField <j.field.2@warwick.ac.uk>
2019-02-08 20:55:19 +0000

    Added the JobApplication model.

--- quikruit/applicants/models.py
+++ quikruit/applicants/models.py
@@ -20,11 +20,8 @@ class PriorEmployment(models.Model):
         related_name = 'prior_employment',
         on_delete = models.CASCADE
     )
-    
     company = models.CharField(max_length=60)
-    
     position = models.CharField(max_length=60)
-    
     length_employment = models.CharField(max_length=30)
     
 class ALevel(models.Model):
@@ -33,9 +30,69 @@ class ALevel(models.Model):
         related_name = 'a_levels',
         on_delete = models.CASCADE
     )
-    
     subject = models.CharField(max_length=40)
-    
     grade = models.CharField(max_length=2)
-    
-# Create your models here.
+
+class SkillHobbyLevel(models.Model):
+    applicant = models.ForeignKey(
+        'applicants.ApplicantProfile',
+        related_name="skills_and_hobbies",
+        on_delete=models.CASCADE
+    )
+
+    skillhobby = models.ForeignKey(
+        'applicants.SkillHobby',
+        related_name="skill_hobby_levels",
+        on_delete=models.CASCADE
+    )
+
+    level_choices = ((i,"{}".format(i)) for i in range(1,6))
+    level = models.IntegerField(choices=level_choices)
+
+class SkillHobby(StringBasedModelIDMixin):
+    name = models.CharField(
+        max_length=40,
+        unique=True
+    )
+    SKILL = 0
+    HOBBY = 1
+    kind_choices = (
+        (SKILL, "Skill"),
+        (HOBBY, "Hobby")
+    )
+    kind = models.IntegerField(choices=kind_choices)
+
+class JobApplication(StringBasedModelIDMixin):
+    job_listing = models.ForeignKey(
+        'recruiters.JobListing',
+        related_name='applications',
+        on_delete=models.CASCADE
+    )
+    applicant = models.ForeignKey(
+        'applicants.ApplicantProfile',
+        related_name='job_applications',
+        on_delete=models.CASCADE
+    )
+    cover_letter = models.TextField(blank=True)
+    date_submitted = models.DateTimeField(null=True)
+
+    IN_PROGRESS            = 0
+    SENT                   = 1
+    UNDER_CONSIDERATION    = 2
+    ONLINE_TEST_REQUESTED  = 3
+    ONLINE_TEST_COMPLETED  = 4
+    REJECTED               = 5
+    OFFER_GIVEN            = 6
+    EMPLOYED               = 7
+
+    status_choices = (
+        (IN_PROGRESS, "In progress"),
+        (SENT, "Application sent"),
+        (UNDER_CONSIDERATION, "Application under consideration"),
+        (ONLINE_TEST_REQUESTED, "Online test requested"),
+        (REJECTED, "No longer being considered"),
+        (OFFER_GIVEN, "Job offer sent"),
+        (EMPLOYED, "Offer accepted")
+    )
+
+
