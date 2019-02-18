5727d3ddc9a533e49d763ebbcf3440e5ade8b303
JordanField <j.field.2@warwick.ac.uk>
2019-02-18 17:17:25 +0000

    added profile picture to homepage

--- quikruit/quikruit/urls.py
+++ quikruit/quikruit/urls.py
@@ -20,4 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('applicants/', include('applicants.urls')),
     path('recruiters/', include('recruiters.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
