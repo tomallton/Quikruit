811449b6d93f933812536c936fb00d64a698debd
alisha-patel <A.Patel.21@warwick.ac.uk>
2019-02-19 12:19:43 +0000

    added to login.css

--- quikruit/applicants/urls.py
+++ quikruit/applicants/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# Applicants homepage. 
+	# Applicants homepage.
 	# URL example: http://quikruit.example/applicants/
 	path('', views.homepage, name='applicants_homepage'),
 
-	# Applicants login page. 
+	# Applicants login page.
 	# URL example: http://quikruit.example/applicants/login/
 	path('login/', views.login, name='applicants_login'),
 
-	# Job application page. 
+	# Job application page.
 	# URL example: http://quikruit.example/applicants/apply/3v4tnf7cgy7yg78/
 	path('apply/<slug:job_id>/', views.homepage, name='applicants_apply')
 ]
