5a684424af395e3df7ca982a2c3ef61528b6cc47
JordanField <j.field.2@warwick.ac.uk>
2019-02-20 00:41:19 +0000

    Some changes to get logging in working on the server.

--- quikruit/applicants/forms.py
+++ quikruit/applicants/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from applicants.models import *
 
-ALevelFormSet = forms.inlineformset_factory(ApplicantProfile, ALevel, extra=0, exclude=[])
-DegreeFormSet = forms.inlineformset_factory(ApplicantProfile, Degree, extra=0, exclude=[])
+ALevelFormSet = forms.inlineformset_factory(ApplicantProfile, ALevel, extra=1, exclude=[])
+DegreeFormSet = forms.inlineformset_factory(ApplicantProfile, Degree, extra=1, exclude=[])
 PriorEmploymentFormSet = forms.inlineformset_factory(ApplicantProfile, PriorEmployment, extra=1, exclude=[])
