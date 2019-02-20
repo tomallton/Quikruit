94f383a6e07465edf45f69ae65aeac42689bc1ee
CopiluStrazilor <47002422+CopiluStrazilor@users.noreply.github.com>
2019-02-20 19:12:37 +0000

    Created the file for score generation

---
+++ quikruit/applicants/algorithm.py
@@ -0,0 +1,10 @@
+def generate_score():
+  if (there are enough labeled applications on the database):
+    return supervised()
+  else:
+    return unsupervised()
+    
+def supervised():
+  
+def unsupervised():
+  
