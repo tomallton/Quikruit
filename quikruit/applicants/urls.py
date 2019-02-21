9eec0dad2369f0999736b52b289ce7229c83d5ee
JordanField <j.field.2@warwick.ac.uk>
2019-02-21 14:07:21 +0000

    fix typo

--- quikruit/applicants/urls.py
+++ quikruit/applicants/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 	# URL Example: http://quikruit.example/job/3v4tnf7cgy7yg78/apply/
 	path('job/apply/', views.application_form, name='applicants_applicationform'),
 
-	path('testing/', views.test_formset_page, name='applicants_testing')
+	path('testing/', views.test_formset_page, name='applicants_testing'),
 
 	path('applicants/register', views.register, name='applicants_register')
 ]
