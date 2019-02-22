61e1ab1d3254b4667a42992a399c4e7e1f23915b
JordanField <j.field.2@warwick.ac.uk>
2019-02-22 16:54:29 +0000

    Typo

--- quikruit/online_tests/urls.py
+++ quikruit/online_tests/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.homepage, name='testing_homepage'),
-	path('<slug:testid>/prepare/', views.preface, name='testing_preface'),
-	path('<slig:testid>/test/' views.test, name='testing_test'),
-	path('<slug:testid>/review/', views.review_test, name='testing_review'),
-	path('<slug:testid>/results/', views.results, name='testing_results')
+	path('<slug:test_id>/prepare/', views.preface, name='testing_preface'),
+	path('<slug:test_id>/test/', views.test, name='testing_test'),
+	path('<slug:test_id>/review/', views.review_test, name='testing_review'),
+	path('<slug:test_id>/results/', views.results, name='testing_results')
 ]
