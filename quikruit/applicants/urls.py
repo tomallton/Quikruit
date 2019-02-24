1943ca65d0d54779ae7eaaf91cf98db7b7161577
alisha-patel <A.Patel.21@warwick.ac.uk>
2019-02-24 17:10:01 +0000

    update base.html/global_homepage.html

--- quikruit/applicants/urls.py
+++ quikruit/applicants/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
 	path('logout/', auth_views.LogoutView.as_view(template_name='applicants/app_logout.html'), name='applicants_logout'),
 
-	# # Job listing page. 
+	# # Job listing page.
 	# # URL example: http://quikruit.example/job/3v4tnf7cgy7yg78/
 	# path('job/<slug:job_id>/', views.homepage, name='applicants_viewjob')
 
@@ -22,4 +22,4 @@ urlpatterns = [
 	path('job/apply/', views.application_form, name='applicants_applicationform'),
 
 	path('register/', views.register, name='applicants_registration')
-]
+]	
