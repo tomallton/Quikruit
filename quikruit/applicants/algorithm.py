54067b53d362cf2b315988577aea490eea670f1f
CopiluStrazilor <47002422+CopiluStrazilor@users.noreply.github.com>
2019-02-25 16:57:02 +0000

    Minor progress

--- quikruit/applicants/algorithm.py
+++ quikruit/applicants/algorithm.py
@@ -1,11 +1,14 @@
+# Programming languages (and other similar CV data) could be represented using one-hot encoding
+
 def generate_score():
-  if (there are enough labeled applications on the database):
+  
+  if (there are enough labeled applications on the system):
     return supervised()
   else:
     return unsupervised()
     
 def supervised():
-  # a logistic regression approach
+  # A logistic regression approach
   
 def unsupervised():
-  
+  # ??
