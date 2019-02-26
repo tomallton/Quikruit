1e6739b2ffc3f4287303ca9af0688436b8e7bd90
JordanField <j.field.2@warwick.ac.uk>
2019-02-26 17:34:02 +0000

    More job listing stuff

--- quikruit/applicants/static/applicants/js/joblisting.js
+++ quikruit/applicants/static/applicants/js/joblisting.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
 	$('.descriptioncollapsebutton').click(function() {
-		console.log('clickbutton')
-		$('.jobdescription').slideToggle('slow');
+		var idregex = /\_[a-z 0-9]*/;
+		var id = idregex.exec($(this).attr('id'))[0];
+		$('#desc' + id).slideToggle('slow');
+		var buttonText = $('#collapse' + id).text();
+		console.log(buttonText);
+		if (buttonText === '▶︎ Description') {
+			buttonText = '▼ Description'
+		} else {
+			buttonText = '▶︎ Description'
+		}
+		console.log(buttonText)
+		$('#collapse' + id).text(buttonText);
 	});
 });
