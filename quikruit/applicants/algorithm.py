30bf9a296e299bb2299131e214c9405d93833bd2
CopiluStrazilor <47002422+CopiluStrazilor@users.noreply.github.com>
2019-02-27 23:18:55 +0000

    Update algorithm.py

--- quikruit/applicants/algorithm.py
+++ quikruit/applicants/algorithm.py
@@ -12,9 +12,10 @@ programming_languages = {
   "PHP": 6,
   "Other": 7
 }
-
+# Computer Science, Computing, Information Technology, Engineering, Electronics
 a_levels = {
-  ""
+  "Computer Science related": 0, # ?
+  
 }
 
 def generate_score():
@@ -29,7 +30,8 @@ def supervised():
   
 # This method will use the job details in order to calculate a score if the supervised learning cannot be used
 # Starting from a score of 50, maximum 50 more points can be added or subtracted
-# If 
+# If a required skill/programming language is not present, a fraction of the maximum will be subtracted (but if it is, do not modify the score)
+# If an optional skill/language is present, a fraction of the maximum will be added (but if it is not, do not modify the score)
 def manual_score():
   starting_score = 50
   maximum_to_add = 50
