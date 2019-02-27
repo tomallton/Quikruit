6b2b4b33ffaf1fe6d9361ac84699530bd4418463
CopiluStrazilor <47002422+CopiluStrazilor@users.noreply.github.com>
2019-02-27 19:05:34 +0000

    Update algorithm.py

--- quikruit/applicants/algorithm.py
+++ quikruit/applicants/algorithm.py
@@ -1,11 +1,20 @@
+import tensorflow as tf
+
 # A dictionary which maps programming languages to their position in a one-hot encoding
-# For example, a person that knows C and Java will have the array [1, 1, 0, 0, 0]
+# For example, a person that knows C and Java will have the array [1, 1, 0, 0, 0, 0, 0, 0]
 programming_languages = {
   "Java": 0,
   "C": 1,
   "C++": 2,
   "Python": 3,
-  "Other": 4
+  "HTML": 4,
+  "JavaScript": 5,
+  "PHP": 6,
+  "Other": 7
+}
+
+a_levels = {
+  ""
 }
 
 def generate_score():
@@ -18,9 +27,12 @@ def generate_score():
 def supervised():
   # A logistic regression approach
   
+# This method will use the job details in order to calculate a score if the supervised learning cannot be used
+# Starting from a score of 50, maximum 50 more points can be added or subtracted
+# If 
 def manual_score():
   starting_score = 50
-  maximum_addition = 50
-  maximum_subtraction = -50
+  maximum_to_add = 50
+  maximum_to_subtract = -50
   
   
