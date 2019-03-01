286c5c73ca4fe3006190f55aa3394047786b0e13
JordanField <j.field.2@warwick.ac.uk>
2019-03-01 14:34:08 +0000

    Tweaks to the test question models

---
+++ quikruit/online_tests/fields.py
@@ -0,0 +1,32 @@
+from django.db import models
+import ast
+
+
+'''
+Taken from https://stackoverflow.com/questions/5216162/how-to-create-list-field-in-django#7394475
+Modified to work with Django 2.0.
+'''
+class ListField(models.TextField):
+    description = "Stores a python list"
+
+    def __init__(self, *args, **kwargs):
+        super(ListField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        if not value:
+            value = []
+
+        if isinstance(value, list):
+            return value
+
+        return ast.literal_eval(value)
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+
+        return unicode(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_db_prep_value(value)
--- quikruit/online_tests/models.py
+++ quikruit/online_tests/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from quikruit.mixins import StringBasedModelIDMixin
+import online_tests.fields as customFields
+from markdownx.models import MarkdownxField
 
 class OnlineTest(StringBasedModelIDMixin):
     application = models.ForeignKey(
@@ -8,25 +10,48 @@ class OnlineTest(StringBasedModelIDMixin):
         on_delete=models.CASCADE
     )
     
-    dateCompleted  = models.DateTimeField(null=True, blank=True)
+    date_completed  = models.DateTimeField(null=True, blank=True)
     
     result = models.FloatField(null=True, blank=True)
     
+# class OnlineTestConfig(models.Model):
+#     job_listing
+
 class TestQuestion(StringBasedModelIDMixin):
-    questionType = models.IntegerField() #TODO: decide on the types
+    MULTI_SELECT = 0
+    SINGLE_SELECT = 1
+    TEXT_ENTRY = 2
+    ANSWER_LABELS = 4
+
+    question_type_choices = (
+        (MULTI_SELECT, 'Multiple Choice (Checkboxes)'),
+        (SINGLE_SELECT,'Multiple Choice (Radio buttons)'),
+        (TEXT_ENTRY, 'Simple text entry')
+    )
+    answer_count_choices = ((i, i) for i in range(1,6))
+    correct_answer_choices = (
+        (0,'A'),
+        (1,'B'),
+        (2,'C'),
+        (3,'D'),
+        (4,'E')
+    )
+
+    question_type = models.IntegerField(choices=question_type_choices)
     
-    testSkill = models.ForeignKey(
+    tested_skill = models.ForeignKey(
         'applicants.SkillHobby',
         related_name = 'question_for_skill',
         on_delete = models.CASCADE,
         null = True
     )
-    
-    question = models.TextField()
-    
-    answers = models.TextField(blank=True)
-    
-    correctAnswer = models.TextField(blank=True)
+
+    question = MarkdownxField()
+    answer_count = models.IntegerField(null=True, choices=answer_count_choices)
+    correct_answer = models.IntegerField(choices=correct_answer_choices)
+
+    def __str__(self):
+        return self.question
     
 class QuestionAnswer(models.Model):
     test = models.ForeignKey(
