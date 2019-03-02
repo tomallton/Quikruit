649c6b22c239e657a4f8f056bc2bacade616bd4c
JordanField <j.field.2@warwick.ac.uk>
2019-03-02 18:37:20 +0000

    Update forms.py

--- quikruit/applicants/forms.py
+++ quikruit/applicants/forms.py
@@ -4,7 +4,7 @@ from applicants.models import *
 ALevelFormSet = forms.inlineformset_factory(ApplicantProfile, ALevel, extra=1, exclude=[])
 DegreeFormSet = forms.inlineformset_factory(ApplicantProfile, Degree, extra=1, exclude=[])
 PriorEmploymentFormSet = forms.inlineformset_factory(ApplicantProfile, PriorEmployment, extra=1, exclude=[])
-SkillHobbyLevelFormSet = forms.inlineformset_factory(ApplicantProfile, SkillHobbyLevel, extra=1, exclude=[])
+SkillHobbyLevelFormSet = forms.inlineformset_factory(ApplicantProfile, SkillHobbyLevel, extra=10, exclude=[])
 
 class ApplicantProfileForm(forms.ModelForm):
 	class Meta:
