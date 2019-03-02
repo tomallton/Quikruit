4f2de7a951f450211d4de1a9bc007d516ead6965
CopiluStrazilor <47002422+CopiluStrazilor@users.noreply.github.com>
2019-03-02 16:33:38 +0000

    Update algorithm.py

--- quikruit/applicants/algorithm.py
+++ quikruit/applicants/algorithm.py
@@ -1,7 +1,9 @@
 # These libraries need to be installed first
 import tensorflow as tf
 import pandas as pd
-import 
+import numpy as np
+
+from tensorflow.python.data import Dataset
 
 # A dictionary which maps programming languages to their position in a one-hot encoding
 # For example, a person that knows C and SQL will have the array [0, 1, 0, 0, 0, 0, 0, 1, 0]
@@ -35,21 +37,24 @@ skills_and_hobbies = {
  # can be excluded? 
 }
 
-def generate_score():
+def generate_score(application):
   
   if (there are enough labeled applications on the system):
-    return supervised_learning()
+    return supervised_learning(application)
   else:
-    return manual_score()
+    return manual_score(application)
     
-def supervised_learning():
+def supervised_learning(application):
   # A logistic regression approach
+  
+  for cv in (applications.all()): #?
+    if cv.(application_status)
   
 # This method will use the job details in order to calculate a score if the supervised learning cannot be used
 # Starting from a score of 50, maximum 50 more points can be added or subtracted
 # If a required skill/programming language is not present, a fraction of the maximum will be subtracted (but if it is, do not modify the score)
 # If an optional skill/language is present, a fraction of the maximum will be added (but if it is not, do not modify the score)
-def manual_score():
+def manual_score(application):
   starting_score = 50
   maximum_to_add = 50
   maximum_to_subtract = -50
