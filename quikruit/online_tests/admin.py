d9ca29eab76b17d9e83b4de37a65ace40fa4f636
Daniel Belo Goncalves <danigo99@gmail.com>
2019-03-02 11:21:30 +0000

    Changed admin site to view online test submissions

--- quikruit/online_tests/admin.py
+++ quikruit/online_tests/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
 from .models import *
+from applicants.models import JobApplication
 
-admin.site.register(OnlineTest)
+#admin.site.register(OnlineTest)
 admin.site.register(TestQuestion)
 admin.site.register(QuestionAnswer)
+     
+class TestQuestionAnswerInLine(admin.TabularInline):
+    model = QuestionAnswer
+    readonly_fields = ['question','answer','correct']
+    
+    def question(self,obj):
+        return format_html(
+			'<a href="{l}">{a} <strong>[Click to view]</strong></a>'.format(
+				l=reverse('admin:online_test_TestQuestion_change', args=[obj.testquestion.model_id]),
+				a=obj.applicant
+			)
+        )
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
+    extra = 0
+
+@admin.register(OnlineTest)
+class OnlineTestAdmin(admin.ModelAdmin):
+    inlines = [
+        #OnlineTestInline,
+        TestQuestionAnswerInLine
+    ]
+    
+    readonly_fields = ['application','date_completed','result']
+    list_display = ('application','date_completed','result')
+    
+    def application(self,obj):
+        return format_html(
+			'<a href="{l}">{a} <strong>[Click to view]</strong></a>'.format(
+				l=reverse('admin:applicants_JobApplication_change', args=[obj.jobapplication.model_id]),
+				a=obj.applicant
+			)
+        )
 
 # Register your models here.
