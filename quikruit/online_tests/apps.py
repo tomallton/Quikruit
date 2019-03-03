30ca61395aadbdc2d43cf22536e9282507133404
JordanField <j.field.2@warwick.ac.uk>
2019-03-03 23:13:45 +0000

    More testing website stuff

--- quikruit/online_tests/apps.py
+++ quikruit/online_tests/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class OnlineTestsConfig(AppConfig):
     name = 'online_tests'
+
+    def ready(self):
+    	import online_tests.signals.handlers
+    	print('signals imported for online_tests')
---
+++ quikruit/online_tests/signals/handlers.py
@@ -0,0 +1,28 @@
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+from online_tests.models import OnlineTest, QuestionResponse, TestQuestion
+from recruiters.models import RequiredSkill
+import random
+import pdb
+
+@receiver(post_save, sender=OnlineTest)
+def online_test_save_handler(sender, **kwargs):
+    test = kwargs['instance']
+
+    # pdb.set_trace()
+    def assign_question(required_skill):
+        suitable_question = random.choice(TestQuestion.objects.filter(tested_skill=required_skill.skill))
+        response = QuestionResponse()
+        response.question = suitable_question
+        response.test = test
+        response.save()
+
+    if not test.question_responses.all():
+        for required_skill in test.application.job_listing.required_skills.all():
+            if required_skill.priority == RequiredSkill.COULD:
+                continue
+            elif required_skill.priority == RequiredSkill.SHOULD:
+                assign_question(required_skill)
+            else:
+                for _ in range(3):
+                    assign_question(required_skill)
