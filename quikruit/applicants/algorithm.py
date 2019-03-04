690a390a57e16220cda05d91a5849dc8ea4c683d
Tom Allton <tomallton1@gmail.com>
2019-03-04 12:30:28 +0000

    Magic score.

--- quikruit/applicants/algorithm.py
+++ quikruit/applicants/algorithm.py
@@ -1,8 +1,33 @@
 
 import math
+from applicants.models import SkillHobby, Feature
 
     def magicScore(applicant):
-        return 1;
+
+        applicantFeatures = set();
+
+        for (aLevel in applicant.a_levels.all()):
+            applicantFeatures.add(str(aLevel))
+
+        for (degree in applicant.degree.all()):
+            applicantFeatures.add(str(degree))
+
+        for (skill in applicant.skills_and_hobbies.filter(kind=SkillHobby.SKILL):
+            applicantFeatures.add(str(skill))
+
+        for (programming_language in applicant.skills_and_hobbies.filter(kind=SkillHobby.PROGRAMMING_LANGUAGE):
+            applicantFeatures.add(str(programming_language))
+
+        for (prior_employment in applicant.prior_employment.all()):
+            applicantFeatures.add(str(prior_employment))
+
+        score = 0
+
+        for (applicantFeature in applicantFeatures):
+            feature_model = Feature.objects.get(name=applicantFeature)
+            score += feature_model.weight
+
+        return sigmoid(score);
 
     # Applies a sigmoid function to a given value.
     #
