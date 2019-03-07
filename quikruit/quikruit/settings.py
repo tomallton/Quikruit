302c6a58eb862ee733c82152f87a7f7627f12e48
JordanField <j.field.2@warwick.ac.uk>
2019-03-07 10:48:18 +0000

    as below

--- quikruit/quikruit/settings.py
+++ quikruit/quikruit/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import sys.platform
 from AWS import is_aws
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
